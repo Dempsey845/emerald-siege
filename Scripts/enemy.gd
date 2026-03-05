428ab6a20749a45092771d35c5d5ca96543329c4
Dempsey845 <dempseynewton1@gmail.com>
2026-03-05 11:05:58 +0000

    Made the enemies look towards their target when stopped

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -7,20 +7,36 @@ const ROTATION_SPEED := 6.0
 
 @onready var navigation_agent: NavigationAgent3D = %NavigationAgent3D
 
-
 func _physics_process(delta: float) -> void:
-	if not navigation_agent.is_target_reachable():
+	if not target_node:
+		return
+
+	if navigation_agent.is_navigation_finished():
+		navigation_agent.set_velocity(Vector3.ZERO)
+		velocity = Vector3.ZERO
+
+		_look_at_target_node(delta)
 		return
 
 	var next_pos = navigation_agent.get_next_path_position()
 	var direction = global_position.direction_to(next_pos)
 
-	var target_angle = atan2(direction.x, direction.z)
-	rotation.y = lerp_angle(rotation.y, target_angle, delta * ROTATION_SPEED)
+	_look_at_move_direction(direction, delta)
 
 	var desired_velocity = direction * MOVE_SPEED
 	navigation_agent.set_velocity(desired_velocity)
 
+
+func _look_at_target_node(delta: float):
+	var look_dir = global_position.direction_to(target_node.global_position)
+	if look_dir.length() > 0.01:
+		var target_angle = atan2(look_dir.x, look_dir.z)
+		rotation.y = lerp_angle(rotation.y, target_angle, delta * ROTATION_SPEED)
+
+func _look_at_move_direction(direction: Vector3, delta: float):
+	if direction.length() > 0.01:
+		var target_angle = atan2(direction.x, direction.z)
+		rotation.y = lerp_angle(rotation.y, target_angle, delta * ROTATION_SPEED)
 
 func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
 	velocity = safe_velocity

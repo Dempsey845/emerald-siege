4427dee635d47a3aa0e0ca0eace2173adb42422e
Dempsey845 <dempseynewton1@gmail.com>
2026-03-05 11:31:27 +0000

    Created a simple attack agent for enemies

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,10 +1,10 @@
-extends CharacterBody3D
+class_name Enemy extends CharacterBody3D
 
 const MOVE_SPEED := 5.0
 const ROTATION_SPEED := 6.0
 
 @export var target_node: Node3D
-
+@export var attack_agent: AttackAgent
 @onready var navigation_agent: NavigationAgent3D = %NavigationAgent3D
 
 func _physics_process(delta: float) -> void:
@@ -16,6 +16,10 @@ func _physics_process(delta: float) -> void:
 		velocity = Vector3.ZERO
 
 		_look_at_target_node(delta)
+		
+		if attack_agent:
+			attack_agent.attack_melee()
+		
 		return
 
 	var next_pos = navigation_agent.get_next_path_position()
@@ -25,7 +29,16 @@ func _physics_process(delta: float) -> void:
 
 	var desired_velocity = direction * MOVE_SPEED
 	navigation_agent.set_velocity(desired_velocity)
+	
+	if attack_agent:
+		attack_agent.attack_ranged()
 
+func get_distance_to_target_node_sq() -> float:
+	if not target_node:
+		push_warning("Attempting to get distance to null target node.")
+		return INF
+	
+	return global_position.distance_squared_to(target_node.global_position)
 
 func _look_at_target_node(delta: float):
 	var look_dir = global_position.direction_to(target_node.global_position)
@@ -41,7 +54,6 @@ func _look_at_move_direction(direction: Vector3, delta: float):
 func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
 	velocity = safe_velocity
 	move_and_slide()
-
 
 func _on_update_nav_timer_timeout() -> void:
 	if not target_node:
---
+++ Scripts/enemy_attack_agent.gd
@@ -0,0 +1,57 @@
+class_name AttackAgent extends Node3D
+
+signal melee_attack
+signal ranged_attack
+
+enum ATTACK_TYPE
+{
+	Melee,
+	Ranged,
+	MeleeAndRanged
+}
+
+@export var attack_type: ATTACK_TYPE
+@onready var enemy: Enemy = get_parent()
+
+@export_category("Ranged")
+@export var ranged_attack_distance: float = 15.0
+@onready var ranged_attack_distance_sq := ranged_attack_distance * ranged_attack_distance
+@onready var ranged_cooldown_timer: Timer = %RangedCooldownTimer
+
+@export_category("RangedAndMelee")
+@export var min_time_between_attack_types: float = 1.0 # The minimum amount of delay between a ranged and melee attack
+
+var time_since_last_melee_attack := 0.0
+var time_since_last_ranged_attack := 0.0
+
+@export_category("Melee")
+@onready var melee_cooldown_timer: Timer = %MeleeCooldownTimer
+
+func _process(delta: float) -> void:
+	time_since_last_melee_attack += delta
+	time_since_last_ranged_attack += delta
+
+func attack_melee():
+	if attack_type != ATTACK_TYPE.Melee or attack_type != ATTACK_TYPE.MeleeAndRanged or not melee_cooldown_timer.is_stopped():
+		return
+	
+	if attack_type == ATTACK_TYPE.MeleeAndRanged and time_since_last_ranged_attack < min_time_between_attack_types:
+		return
+
+	melee_attack.emit()
+	time_since_last_melee_attack = 0.0
+	melee_cooldown_timer.start()
+	
+func attack_ranged():
+	if attack_type != ATTACK_TYPE.Ranged or attack_type != ATTACK_TYPE.MeleeAndRanged:
+		return
+		
+	if not ranged_cooldown_timer.is_stopped() or enemy.get_distance_to_target_node_sq() > ranged_attack_distance_sq:
+		return
+		
+	if attack_type == ATTACK_TYPE.MeleeAndRanged and time_since_last_melee_attack < min_time_between_attack_types:
+		return
+	
+	ranged_attack.emit()
+	time_since_last_ranged_attack = 0.0
+	ranged_cooldown_timer.start()

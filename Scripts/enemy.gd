afe19d97a58e72fd356a166f872bb3dbdbe9e3f5
Dempsey845 <dempseynewton1@gmail.com>
2026-03-05 10:49:04 +0000

    Added avoidance to the enemy agents

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -6,17 +6,26 @@ const MOVE_SPEED := 5.0
 
 @onready var navigation_agent: NavigationAgent3D = %NavigationAgent3D
 
+
 func _physics_process(_delta: float) -> void:
 	if not navigation_agent.is_target_reachable():
 		return
-	
-	var direction = global_position.direction_to(navigation_agent.get_next_path_position())
-	velocity = direction * MOVE_SPEED
-	
+
+	var next_pos = navigation_agent.get_next_path_position()
+	var direction = global_position.direction_to(next_pos)
+
+	var desired_velocity = direction * MOVE_SPEED
+
+	navigation_agent.set_velocity(desired_velocity)
+
+
+func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
+	velocity = safe_velocity
 	move_and_slide()
-	
+
+
 func _on_update_nav_timer_timeout() -> void:
 	if not target_node:
 		return
-	
+
 	navigation_agent.target_position = target_node.global_position

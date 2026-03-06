cc3701585b95c00cac8cd8f0c398195f478eada8
Dempsey845 <dempseynewton1@gmail.com>
2026-03-06 16:37:22 +0000

    Gave enemy an idle animation

--- Scripts/barbarian.gd
+++ Scripts/barbarian.gd
@@ -10,20 +10,20 @@ extends Node3D
 @export var hit_duration: float = 1.0
 var hit_timer: float = 0.0
 
-var run_attack_blend := 0.0
-var target_blend := 0.0
+var attack_blend := 0.0
+var target_attack_blend := 0.0
+var move_blend := 0.0
+var target_move_blend := 0.0
 var blend_speed := 5.0
 
 var is_attacking := false
 
 @onready var enemy_attack_agent: AttackAgent = %EnemyAttackAgent
 
-
 func _ready():
 	get_parent().damage_taken.connect(trigger_hit)
 	enemy_attack_agent.melee_attack.connect(run_to_attack)
 	enemy_attack_agent.melee_ended.connect(attack_to_run)
-
 
 func _process(delta):
 	# Update hit shader effect
@@ -33,8 +33,12 @@ func _process(delta):
 			mesh.set_instance_shader_parameter("time_passed", hit_timer)
 
 	# Smoothly blend run and attack
-	run_attack_blend = lerp(run_attack_blend, target_blend, delta * blend_speed)
-	%AnimationTree.set("parameters/RunAttackBlendTree/RunAttackBlend/blend_amount", run_attack_blend)
+	attack_blend = lerp(attack_blend, target_attack_blend, delta * blend_speed)
+	%AnimationTree.set("parameters/IdleAttackBlendTree/IdleAttackBlend/blend_amount", attack_blend)
+	%AnimationTree.set("parameters/RunAttackBlendTree/RunAttackBlend/blend_amount", attack_blend)
+	
+	move_blend = lerp(move_blend, 0.0 if get_parent().velocity == Vector3.ZERO else 1.0, delta * blend_speed)
+	%AnimationTree.set("parameters/IdleRunAttackBlend/blend_amount", move_blend)
 
 
 func run_to_attack():
@@ -42,12 +46,12 @@ func run_to_attack():
 		return
 
 	is_attacking = true
-	target_blend = 1.0 
+	target_attack_blend = 1.0 
 
 
 func attack_to_run():
 	is_attacking = false
-	target_blend = 0.0 
+	target_attack_blend = 0.0 
 
 
 func trigger_hit():

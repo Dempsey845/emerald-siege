f79fbf406aba0796bcd2547d3687bdd516833358
Dempsey845 <dempseynewton1@gmail.com>
2026-03-07 17:30:45 +0000

    Created a new enemy AI 'type' called radial, designed for ranged enemies, moves to a random point within a given radius of the target and attacks until the target goes out of distance

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -5,17 +5,29 @@ signal damage_taken
 const MOVE_SPEED := 7.0
 const ROTATION_SPEED := 6.0
 
+enum AI_Type
+{
+	Chase,
+	Radial
+}
+
+@export var ai_type: AI_Type = AI_Type.Chase
 @export var target_node: Node3D
 @export var attack_agent: AttackAgent
+@export var radial_agent: RadialAgent
+
 @onready var navigation_agent: NavigationAgent3D = %NavigationAgent3D
 
 var health := 3
+
+func _ready() -> void:
+	await get_tree().create_timer(0.2).timeout
 
 func _physics_process(delta: float) -> void:
 	if not target_node:
 		return
 
-	if navigation_agent.is_navigation_finished():
+	if ai_type == AI_Type.Chase and navigation_agent.is_navigation_finished():
 		navigation_agent.set_velocity(Vector3.ZERO)
 		velocity = Vector3.ZERO
 
@@ -28,15 +40,19 @@ func _physics_process(delta: float) -> void:
 
 	var next_pos = navigation_agent.get_next_path_position()
 	var direction = global_position.direction_to(next_pos)
-
-	_look_at_move_direction(direction, delta)
+	
+	if ai_type == AI_Type.Radial:
+		radial_agent.process_radial(delta)
+	elif ai_type == AI_Type.Chase:
+		look_at_direction(direction, delta)
+		
+		if attack_agent:
+			attack_agent.attack_ranged()
+			attack_agent.attack_melee()
 
 	var desired_velocity = direction * MOVE_SPEED
 	navigation_agent.set_velocity(desired_velocity)
 	
-	if attack_agent:
-		attack_agent.attack_ranged()
-		attack_agent.attack_melee()
 
 func get_distance_to_target_node_sq() -> float:
 	if not target_node:
@@ -51,7 +67,7 @@ func _look_at_target_node(delta: float):
 		var target_angle = atan2(look_dir.x, look_dir.z)
 		rotation.y = lerp_angle(rotation.y, target_angle, delta * ROTATION_SPEED)
 
-func _look_at_move_direction(direction: Vector3, delta: float):
+func look_at_direction(direction: Vector3, delta: float):
 	if direction.length() > 0.01:
 		var target_angle = atan2(direction.x, direction.z)
 		rotation.y = lerp_angle(rotation.y, target_angle, delta * ROTATION_SPEED)
@@ -64,7 +80,13 @@ func _on_update_nav_timer_timeout() -> void:
 	if not target_node:
 		return
 
-	navigation_agent.target_position = target_node.global_position
+	if radial_agent:
+		radial_agent.target_node = target_node
+		radial_agent.distance_from_target_sq = global_position.distance_squared_to(target_node.global_position)
+	
+	if ai_type == AI_Type.Chase:
+		navigation_agent.target_position = target_node.global_position
+		
 
 func take_damage(damage: int):
 	health -= damage
---
+++ Scripts/radial_agent.gd
@@ -0,0 +1,91 @@
+class_name RadialAgent extends Node3D
+
+enum AI_Radial_State
+{
+	FindingPoint,
+	GoingToPoint,
+	Attacking,
+}
+
+@export_category("Radial") 
+@export var attack_agent: AttackAgent
+@export var max_distance_from_target: float = 15.0
+@export var attack_distance_from_target: float = 10.0
+@onready var max_distance_from_target_sq := max_distance_from_target * max_distance_from_target
+
+var radial_state := AI_Radial_State.FindingPoint
+var radial_point := Vector3.ZERO
+var min_attack_time := 2.0 # Minimum amount of time an enemy has to attack for at a radial point before moving on
+var attack_time = 0.0
+var map_ready := false
+
+var distance_from_target_sq: float = INF
+var target_node: Node3D
+
+@onready var navigation_agent: NavigationAgent3D = %NavigationAgent3D
+
+func _ready() -> void:
+	await get_tree().create_timer(1.0).timeout
+	map_ready = true
+
+func is_nav_ready():
+	var map = get_world_3d().navigation_map
+	return NavigationServer3D.map_get_iteration_id(map) > 0
+
+func get_random_point_near_target(radius: float) -> Vector3:
+	if not target_node or not is_nav_ready():
+		return Vector3.ZERO
+		
+	# Random direction
+	var dir = Vector3(
+		randf_range(-1.0, 1.0),
+		0,
+		randf_range(-1.0, 1.0)
+	).normalized()
+
+	# Random distance within radius
+	var distance = randf_range(0.0, radius)
+
+	var random_point = target_node.global_position + dir * distance
+
+	# Snap to nearest navigation mesh point
+	return NavigationServer3D.map_get_closest_point(
+		navigation_agent.get_navigation_map(),
+		random_point
+	)
+
+func process_radial(delta: float):
+	if not map_ready or not target_node:
+		return
+	
+	match radial_state:
+		AI_Radial_State.FindingPoint:
+			_find_point()
+		AI_Radial_State.GoingToPoint:
+			_go_to_point()
+		AI_Radial_State.Attacking:
+			_attack(delta)
+
+func _find_point():
+	radial_point = get_random_point_near_target(attack_distance_from_target)
+	print(radial_point)
+	navigation_agent.target_position = radial_point
+	radial_state = AI_Radial_State.GoingToPoint
+	
+func _go_to_point():
+	if navigation_agent.is_navigation_finished():
+				radial_state = AI_Radial_State.Attacking
+				navigation_agent.target_position = global_position
+
+func _attack(delta: float):
+	attack_time += delta
+	if attack_time > min_attack_time:
+		if distance_from_target_sq > max_distance_from_target_sq:
+			radial_state = AI_Radial_State.FindingPoint
+			attack_time = 0.0
+			return
+			
+	if attack_agent:
+		get_parent().velocity = Vector3.ZERO
+		get_parent().look_at_direction(global_position.direction_to(target_node.global_position), delta)
+		attack_agent.attack_ranged()

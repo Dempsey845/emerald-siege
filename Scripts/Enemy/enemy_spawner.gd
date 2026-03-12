dae838ba4139b06cf2e74fd338e6d1ef95084e94
Dempsey845 <dempseynewton1@gmail.com>
2026-03-12 14:09:59 +0000

    Enabled local to scene for emerald repo material

--- Scripts/Enemy/enemy_spawner.gd
+++ Scripts/Enemy/enemy_spawner.gd
@@ -5,12 +5,15 @@ const BARBARIAN_ENEMY = preload("res://Scenes/Enemy/Characters/barbarian_enemy.t
 @export var enemy_container: Node3D
 @onready var path_progress: PathFollow3D = %PathProgress
 
+var max_enemies := 50.0
+
 func get_random_spawn_position():
 	path_progress.progress_ratio = randf()
 	return path_progress.global_position
 
 
 func _on_spawn_timer_timeout() -> void:
-	var enemy = BARBARIAN_ENEMY.instantiate()
-	enemy_container.add_child(enemy)
-	enemy.global_position = get_random_spawn_position()
+	if enemy_container.get_child_count() < max_enemies:
+		var enemy = BARBARIAN_ENEMY.instantiate()
+		enemy_container.add_child(enemy)
+		enemy.global_position = get_random_spawn_position()
